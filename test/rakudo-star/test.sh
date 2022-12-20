4e6405a382a02eb312b4aa0d33a97a61a6a23234
Daniel Mita <noreply@dango.space>
2022-12-20 21:27:02 +0000

    Change check to raku version

--- test/rakudo-star/test.sh
+++ test/rakudo-star/test.sh
@@ -3,8 +3,7 @@ cd $(dirname "$0")
 source test-utils.sh
 
 # Template specific tests
-check "distro" lsb_release -c
-check "greeting" [ $(raku -e '"Hello, World!"' | grep 'Hello, World!') ]
+check "raku" raku --version
 
 # Report result
 reportResults
